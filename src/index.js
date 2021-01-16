ca9867927658e34a59f4413f37a2055cd8efed5b
Savannaha <savwiley@yahoo.com>
2021-01-16 13:17:15 -0600

    fixed localStorage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import {toDos, storage} from "./modules/init";
 import runDoms from "./modules/doms";
 import {newCard} from "./modules/forms";
-import projectsRun from "./modules/projects";
-import {storageProj} from "./modules/projects";
+import projectsRun, {storageProj} from "./modules/projects";
 
 
 
